c6ca1fd3b77db0df00710cffca2847cc1ada8ac4
Ntshegi <ntshegg@lhc.local>
2017-01-24 14:46:42 +0200

    'project'

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tangentApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tangentApp
+ */
+angular.module('tangentApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tangentApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tangentApp
+ */
+angular.module('tangentApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
