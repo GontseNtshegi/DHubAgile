7652001e3020efa8a32b1e3150322fe3eb64727c
Ntshegi <ntshegg@lhc.local>
2017-01-25 13:36:22 +0200

    Appchanged

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tangentApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the tangentApp
+ */
+angular.module('tangentApp')
+  .controller('LoginCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
